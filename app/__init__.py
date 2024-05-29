6d277bc722e64d41e20b538153df0681e62d4d86
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-05-28 22:16:49 -0300

    adiciona comentários no init do app

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,14 @@ load_dotenv()
 
 
 def create_app():
-    """A criação da aplicação Flask é feita com o método fábrica
+    """Criação da aplicação Flask 
+
+    A aplicação é criada com o método fábrica
+    e uso de blueprints (por hora só tem uma)
     """
     app = Flask(__name__)
 
+    # adiciona a configuração com o enderço do banco de dados
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
     # Inicializa as instâncias do SqlAlchemy e do Flask Migrate
@@ -19,6 +23,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # registra a(s) blueprint(s)
     app.register_blueprint(bp_games)
 
     return app
