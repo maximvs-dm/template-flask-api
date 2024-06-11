ee1e44d9304f7252709cf9d677be50aacccdd658
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-06-11 20:37:05 -0300

    adiciona verificação e validação na busca do cep

--- app/services/viacep.py
+++ app/services/viacep.py
@@ -1,17 +1,15 @@
 from requests import get
+import re
+
+cep_pattern = re.compile(r'^\d{2}\.?\d{3}-?\d{3}$', re.IGNORECASE | re.MULTILINE)
+not_number_pattern = re.compile(r'\D')
 
 
 def get_address(cep):
-    response = get(f'https://viacep.com.br/ws/{cep}/json/')
+    if not cep_pattern.match(cep):
+        raise Exception(f'Cep com formato inválido: {cep}')
+
+    cep_numbers = not_number_pattern.sub('', cep)
+
+    response = get(f'https://viacep.com.br/ws/{cep_numbers}/json/')
     return response.json()
-
-class Viacep:
-    def __init__(self, param):
-        self.param = param
-        self.key = 
-
-    def get_address(self, bla):
-        get(f'{self.param}/{bla}')
-
-    def post_address(self, cep, data):
-        pass
