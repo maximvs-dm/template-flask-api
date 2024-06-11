6ef66ab0a9d2f08e4689d066518e14065387c839
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-06-11 20:36:41 -0300

    inclui relações nos modelos

--- app/models/addresses.py
+++ app/models/addresses.py
@@ -4,14 +4,7 @@ from models import db
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cep = db.Column(db.String(9))
-
-
-def teste(p):
-    """ Resumo da função
-
-    param: p
-    return: None
-    raises: 
-        KeyError
-    """
-
+    logradouro = db.Column(db.String(255))
+    bairro = db.Column(db.String(255))
+    cidade = db.Column(db.String(255))
+    estado = db.Column(db.String(50))
--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,23 @@
 from . import db
 
+addresses = db.Table(
+    'addresses',
+    db.Column('user_id', db.Integer, db.ForeignKey('address.id'), primary_key=True),
+    db.Column('address_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+games = db.Table(
+    'addresses',
+    db.Column('user_id', db.Integer, db.ForeignKey('address.id'), primary_key=True),
+    db.Column('game_id', db.Integer, db.ForeignKey('game.id'), primary_key=True)
+)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True)
+    address = db.relationship('Address', secondary=addresses, lazy='subquery',
+                              backref=db.backref('users', lazy=True))
+    games = db.relationship('Game', secondary=games, lazy='subquery',
+                            backref=db.backref('users', lazy=True))
